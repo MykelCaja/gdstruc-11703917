9c00fb4af29fffeaae22c112fe760d30a3c41602
MykelCaja <mykelangelomacario.caja@benilde.edu.ph>
2021-09-16 17:07:20 +0800

    Removed the character creation in Module3. Still doesn't add in the Array Queue.

--- Module3/src/com/cajamykel/Main.java
+++ Module3/src/com/cajamykel/Main.java
@@ -6,59 +6,39 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayQueue queue = new ArrayQueue(15);
-        int round = 1;
+        int round = 0;
         int random = 0;
         int game = 0;
 
         while(game <= 10){
-
             random = 1 + (int)(Math.random() * ((5 - 1) + 1));
-            System.out.println("Players added: [" + random + "]");
+            System.out.println("Players to be added: [" + random + "]");
             System.out.println();
 
             //Character creation:
-            for(int i = 1; i == random; i++){
+            for(int i = 0; i == random; i++){
                 System.out.println("==========[NEW PLAYER ADDED]==========");
                 int id = 1000000 + (int)(Math.random() * ((9999999 - 1000000) + 1000000));
                 int level = 1 + (int)(Math.random() * ((100 - 1) + 1));
-                int number = 1 + (int)(Math.random() * ((5 - 1) + 1));
-                String color = null;
-
-                if(number == 1){
-                    color = "Red";
-                }
-
-                if(number == 2){
-                    color = "Blue";
-                }
-
-                if(number == 3){
-                    color = "Yellow";
-                }
-
-                if(number == 4){
-                    color = "Pink";
-                }
-
-                if(number == 5){
-                    color = "Black";
-                }
-
-                queue.add(new Player(id,color,level));
-                queue.peek();
+                queue.add(new Player(id,"BOT",level));
             }
 
             //Print the queue:
+            System.out.println("TOTAL PLAYERS: [" + queue.size() + "]");
             queue.printQueue();
 
             //Initiate game:
             if(queue.size() < 5){
+                System.out.println("NOT ENOUGH PLAYERS IN THE LOBBY.");
+                System.out.println("PLEASE ADD MORE.");
+            }
+
+            else{
                 System.out.println("==========[GAME INITIATED GET READY]==========");
                 for(int i = 0; i < 5; i++){
                     if(queue.size() <= 0){
                         System.out.println("[NO PLAYER AVAILABLE]");
                     }
-
                     else{
                         System.out.println("[PLAYER REMOVED SUCCESSFUL]");
                         queue.remove();
